8dff6d1a8637237636aaa9e61da167c516099d16
Max Lythgoe <lythgoemax@gmail.com>
2022-06-23 13:31:51 -0600

    pivot index

--- Coding-Challenges.js
+++ Coding-Challenges.js
@@ -10,3 +10,23 @@ var runningSum = function(nums) {
     return result
 };
 //
+
+// 724. Find Pivot Index
+var pivotIndex = function(nums) {
+    let totalSum = 0;
+    
+    for (let i=0; i < nums.length; i++) {
+        totalSum += nums[i];
+    }
+    
+    let leftSum = 0;
+    for (let i=0; i<nums.length; i++) {
+        if (i != 0) leftSum += nums[i-1];
+        if (totalSum - leftSum - nums[i] == leftSum) {
+            return i;
+        }
+    }
+    
+  return -1;
+};
+//
