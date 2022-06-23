0efd39194586737dc817c22eddb820337c13426f
Max Lythgoe <lythgoemax@gmail.com>
2022-06-23 12:46:55 -0600

    first commit

---
+++ Coding-Challenges.js
@@ -0,0 +1,12 @@
+// 1480 Running Sum of 1d Array
+var runningSum = function(nums) {
+    let total = 0
+    let result = []
+    
+    for (let i = 0; i < nums.length; i++) {
+        total = total + nums[i]
+        result.push(total)
+    }
+    return result
+};
+//
