cd86a1037680788091b5edc50f89601bc43695ba
Max Lythgoe <lythgoemax@gmail.com>
2022-09-06 18:34:31 -0600

    added typing animation

--- PORTFOLIO/netlify-portfolio/netlify-portfolio/src/Components/Header.jsx
+++ PORTFOLIO/netlify-portfolio/netlify-portfolio/src/Components/Header.jsx
@@ -1,23 +1,24 @@
-import React from 'react'
-import "./Header.css"
-import mainPhoto from './pics/max-logo-white.png'
+import React from "react";
+import "./Header.css";
+import mainPhoto from "./pics/max-logo-white.png";
 
 const Header = () => {
-
   return (
-    <div className='main-container'>
-    <img src={mainPhoto} alt='webdev'></img>
-    <h1 className='main-title'>MAX LYTHGOE</h1>
-    <p className='web-sub'>WEB DEVELOPER</p>
-    <hr></hr>
-    <div className='links-container'>
-    <a href="#contact">CONTACT</a>
-    <a href="#projects">PROJECTS</a>
-    <a href="#skills">SKILLS</a>
-    <a href="#about">ABOUT</a>
+    <div className="main-container">
+      <img src={mainPhoto} alt="webdev"></img>
+      <h1 className="main-title">MAX LYTHGOE</h1>
+      <div className="wrapper">
+        <div className="typing-demo">web developer</div>
+      </div>
+      <hr></hr>
+      <div className="links-container">
+        <a href="#contact">CONTACT</a>
+        <a href="#projects">PROJECTS</a>
+        <a href="#skills">SKILLS</a>
+        <a href="#about">ABOUT</a>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
