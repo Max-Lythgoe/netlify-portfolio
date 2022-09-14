6b2b81a35179152ddbdbe76b66ca963fe1422521
Max Lythgoe <lythgoemax@gmail.com>
2022-09-14 08:44:49 -0600

    sum multiples 3 or 5

--- Challenges/Coding-Challenges.js
+++ Challenges/Coding-Challenges.js
@@ -124,3 +124,17 @@ function findNumber(arr, k) {
       return result;
   }
 //
+
+// sum multiples of 3 or 5 //
+function solution(number){
+  
+    let sum = 0;
+    
+    for(let i = 1; i < number; i++) {
+      if(i%3 === 0 || i%5 ===0) {
+        sum += i;
+      }
+    }
+    return sum
+}
+//
