9c85405c8d182e91a99b27e41f1c89fb809b6d92
Max Lythgoe <lythgoemax@gmail.com>
2022-10-21 16:53:51 -0600

    count divisors

--- Challenges/Coding-Challenges.js
+++ Challenges/Coding-Challenges.js
@@ -202,3 +202,16 @@ function enough(cap, on, wait) {
   }
 }
 //
+
+// Count the divisors of a number //
+function getDivisorsCnt(n){
+  let count = 0;
+
+for (let i = 1; i <= n; i++) {
+  if (n % i == 0) {
+    count += 1;
+  }
+}
+return count
+}
+//
